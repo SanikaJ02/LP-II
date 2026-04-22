69ef92e501fd22491f9b21b33b95f95d01a0976e
Sanika Jagtap <162898897+SanikaJ02@users.noreply.github.com>
2026-04-22 18:58:13 +0530

    Initialize Express server with user routes

---
+++ Assignment 3/Part B/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+app.use(express.json());
+connectDB();
+app.use("/users", require("./routes/userroutes"));
+app.get("/", (req, res) => {
+  res.send("Server Running...");
+});
+
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
